e1e625e3fee0341c1c572740406ff77639139479
roctbb <roctbb@gmail.com>
2020-10-20 17:49:13 +0300

    fix uuid

--- ctrack-bot.py
+++ ctrack-bot.py
@@ -154,7 +154,7 @@ def receiver():
                     new_data = ctrack_api.get_data(contract.access_token, last_id=contract.last_id)
 
                     for item in new_data:
-                        timestamp = datetime.datetime.strptime(item['measured_dt'][:-3], "%Y-%m-%dT%H:%M:%S.%fZ")
+                        timestamp = datetime.datetime.strptime(item['measured_dt'][:19], "%Y-%m-%dT%H:%M:%S")
                         timestamp += datetime.timedelta(hours=-4)
                         timestamp.timestamp()
 
